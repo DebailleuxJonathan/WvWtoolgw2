ad6231ef246cb58392a2f55ae9f446fdd633a461
DebailleuxJonathan <jonathan.debailleux@edu.itescia.fr>
2020-08-18 14:19:41 +0200

    file upload to DB

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,15 @@
+console.log('aya');
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+)
+
+FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100 ,
+    imageResizeTargetHeight: 150
+
+})
+
+FilePond.parse(document.body);
